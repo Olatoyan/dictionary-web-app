e1ddc6aa7601c58728403d0be630f224ad67ee4a
Olatoyan <golatoyan@gmail.com>
2023-07-06 23:26:13 +0100

    initial commit

---
+++ starter-code/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const selectFonts = document.querySelector(".select__icon");
+const fontsBox = document.querySelector(".fonts__box ");
+
+// https://api.dictionaryapi.dev/api/v2/entries/en/<word>
